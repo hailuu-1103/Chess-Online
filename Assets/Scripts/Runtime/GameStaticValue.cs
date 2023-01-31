5c35b8083a3830b2b738be0a75b600553affb9b8
hailuu <hoanghai.luu.71@gmail.com>
2023-01-31 13:39:30 +0700

    refactor code, write back-holder for promotion

--- Assets/Scripts/Runtime/GameStaticValue.cs
+++ Assets/Scripts/Runtime/GameStaticValue.cs
@@ -2,8 +2,9 @@ namespace Runtime
 {
     public static class GameStaticValue
     {
-        public static float TileOffsetY = 0.15f;
-        public static int   BoardRows   = 8;
-        public static int   BoardColumn = 8;
+        public static float TileOffsetY  = 0.15f;
+        public static int   BoardRows    = 8;
+        public static int   BoardColumn  = 8;
+        public static float MoveDuration = 1f;
     }
 }
